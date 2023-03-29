53c5c6b98bfc23142a5f89a32f14fb7f46caeaa7
kimhosung <70985410+hosung1998@users.noreply.github.com>
2023-03-29 15:52:51 +0900

    실행 IP 변경

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { WebView } from 'react-native-webview';
 export default class App extends React.Component {
   render() {
     //uri : 웹뷰 주소
-    return <WebView source={{ uri: 'https://web-chat-gpt-p8xrq2mlff30dde.sel3.cloudtype.app' }} style={{ marginTop: 0 }} />;
+    return <WebView source={{ uri: 'http://192.168.1.56:3000/ScheduleCreation' }} style={{ marginTop: 0 }} />;
   }
 }
 
