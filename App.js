b338776be83ebc8e0face5cfe372138b06129b90
parkdaechul <eocjf4701@gmail.com>
2023-04-09 14:21:05 +0900

    splash 화면 및 기능 구현

--- App.js
+++ App.js
@@ -1,10 +1,21 @@
 import * as React from 'react';
 import { WebView } from 'react-native-webview';
+import Loading from "./src/load/Loading";
 
 export default class App extends React.Component {
+  state = {
+    isLoading: true
+  };
+  componentDidMount = async () => {
+    setTimeout(() => { this.setState({ isLoading: false }) }, 3000);
+  }
   render() {
+    // splash 띄우기
+    if (this.state.isLoading) {
+      return <Loading />
+    }
     //uri : 웹뷰 주소
-    return <WebView source={{ uri: 'http://192.168.1.56:3000/ScheduleCreation' }} style={{ marginTop: 0 }} />;
+    return <WebView source={{ uri: 'http://192.168.0.7:3000/mypage' }} style={{ marginTop: 0 }} />;
   }
 }
 
